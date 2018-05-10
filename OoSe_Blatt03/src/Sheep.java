1a807a4366a96202819105c3c23d538537fa8f84
Lukas Storck <lukas.storck@gmx.de>
2018-05-10 18:51:16 +0200

    + einen Teil der Dateien für Aufgabenblatt 3
    noch nicht bearbeitet, nur was in der Aufgabenstellung gegeben ist...

---
+++ OoSe_Blatt03/src/Sheep.java
@@ -0,0 +1,22 @@
+
+public class Sheep {
+	public String name;
+	public Fur fur;
+
+	public Sheep(String name, Fur fur) {
+		this.name = name;
+		this.fur = fur;
+	}
+
+	public void shear() {
+		fur.length = 0;
+	}
+
+	public String toString() {
+		return "Name: " + name + " " + fur.toString();
+	}
+
+	public Sheep clone() {
+		return this;
+	}
+}
---
+++ OoSe_Blatt03/src/Test.java
@@ -0,0 +1,15 @@
+
+public class Test {
+	public static void main(String[] args) {
+//		C1 a = new C2();
+//		C2 b = new C2();
+//		((C2) a).f1();
+//		((C1) b).f1();
+//		System.out.println(a.s);
+//		b.f1();
+//		b.f2();
+//		b.f3();
+//		C1 c= new C1(); c.f3();
+//		C1 c= new C1(); ((C2) c).f1();
+	}
+}
