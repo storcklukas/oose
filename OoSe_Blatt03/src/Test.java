66cac9f69b3e97535fe030959b8c0853b5ee47e5
Lukas Storck <lukas.storck@gmx.de>
2018-05-12 18:57:50 +0200

    + Blatt 3 Aufgabe 1

--- OoSe_Blatt03/src/Test.java
+++ OoSe_Blatt03/src/Test.java
@@ -1,8 +1,19 @@
-
+/*
+ * Ausgaben:
+ * 1) C2::f1
+ * 2) C2::f1
+ * 3) 1
+ * 4) C2::f1
+ * 5) C1::f2
+ * 6) C2::f3
+ * 7) Exception in thread "main" java.lang.Error: Unresolved compilation problem: The method f3() is undefined for the type C1 at Test.main(Test.java:23)
+ * 8) Exception in thread "main" java.lang.ClassCastException: C1 cannot be cast to C2 at Test.main(Test.java:24)
+ * 
+ */
 public class Test {
 	public static void main(String[] args) {
-//		C1 a = new C2();
-//		C2 b = new C2();
+		C1 a = new C2();
+		C2 b = new C2();
 //		((C2) a).f1();
 //		((C1) b).f1();
 //		System.out.println(a.s);
