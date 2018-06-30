e4f93b5214de77ad3f5ffbb3de51cf1d593d7269
storcklukas <lukas.storck@gmx.de>
2018-07-01 00:15:26 +0200

    + OoSe_Blatt07 Aufgabe 04

---
+++ OoSe_Blatt07/src/aufgabe04_python/aufgabe04.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+
+def finde_primzahlen(max_number=1000):
+    prim_list = list()
+    prim_list.append(2)
+
+    for n in range(3, max_number + 1):
+        is_prim = True
+        for prim in prim_list:
+            if n % prim == 0:
+                is_prim = False
+                break
+        if is_prim:
+            prim_list.append(n)
+            print(n)
+        n += 1
+
+
+finde_primzahlen()
